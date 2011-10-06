28f915db90f2009aebd8983c6173906ab86f311b
Graeme Benstead-Hume <graeme@jumpdigital.co.uk>
2011-10-06 20:39:41 +0100

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mymincepie::Application.config.secret_token = 'ff0f1cca189ea9d7f7b3a594def76febd7f0d117cde2c7c4551bda70c969531f1c47d00aa26ebac3635f45906bf5fc6d1b14941314c5052daf3d8208e54684d8'

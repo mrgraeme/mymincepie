ff1254dc2369b40db3a87ea1c04e14f6f6b23f26
Graeme Benstead-Hume <graeme@jumpdigital.co.uk>
2011-10-27 22:21:44 +0100

    User sessions complete

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -90,11 +90,18 @@ describe UsersController do
         post :create, :user => @attr
         response.should redirect_to(user_path(assigns(:user)))
       end
-      
+
       it 'should have a welcome message' do
         post :create, :user => @attr
         flash[:success].should =~ /Thank you for joining us at MyMincepie.com/i
-      end    
+      end   
+
+      it "should sign the user in" do
+        post :create, :user => @attr
+        controller.should be_signed_in
+      end
+
+
     end
   end
 end
